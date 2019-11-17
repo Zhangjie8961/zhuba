0268c12bea30bc5d434a1b7942c0e57dbe1c7219
Zhangjie8961 <896180039@qq.com>
2019-11-17 18:28:56 +0800

    zhuba

---
+++ build/precache-manifest.2b8b371b795abd3d214f704dc55f7a0a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6a497bd524bdcf4cb912036167db1850",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5a52e9b4ae8b9f1895a5",
+    "url": "/static/css/2.6f60e7f3.chunk.css"
+  },
+  {
+    "revision": "ee071476adf5997fa5dd",
+    "url": "/static/css/main.63492f0f.chunk.css"
+  },
+  {
+    "revision": "5a52e9b4ae8b9f1895a5",
+    "url": "/static/js/2.3bfe0f54.chunk.js"
+  },
+  {
+    "revision": "ee071476adf5997fa5dd",
+    "url": "/static/js/main.3c17d356.chunk.js"
+  },
+  {
+    "revision": "14c23a88433f623ab3ae",
+    "url": "/static/js/runtime-main.e2c7d5a8.js"
+  }
+]);
